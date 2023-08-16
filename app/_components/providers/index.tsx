c6e50eddb0fb372294ce9769e4e7917c9aecac63
im-luka <lux.9833@gmail.com>
2023-08-16 15:03:45 +0200

    feat: install & setup react-query & devtools

--- app/_components/providers/index.tsx
+++ app/_components/providers/index.tsx
@@ -1,5 +1,9 @@
+"use client";
+
 import { FC, ReactNode } from "react";
 import { NextIntlClientProvider } from "next-intl";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { ThemeProvider } from "./theme";
 
 type Props = {
@@ -10,9 +14,14 @@ type Props = {
 export const Providers: FC<Props> = async ({ locale, children }) => {
   const messages = (await import(`@/messages/${locale}.json`)).default;
 
+  const queryClient = new QueryClient();
+
   return (
     <NextIntlClientProvider locale={locale} messages={messages}>
-      <ThemeProvider>{children}</ThemeProvider>
+      <QueryClientProvider client={queryClient}>
+        <ReactQueryDevtools initialIsOpen={false} />
+        <ThemeProvider>{children}</ThemeProvider>
+      </QueryClientProvider>
     </NextIntlClientProvider>
   );
 };
