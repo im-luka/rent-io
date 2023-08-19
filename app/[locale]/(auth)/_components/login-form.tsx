a1487f62250b234c062c8196930e2ffae4710986
im-luka <lux.9833@gmail.com>
2023-08-19 17:45:10 +0200

    feat: login page ui

--- app/[locale]/(auth)/_components/login-form.tsx
+++ app/[locale]/(auth)/_components/login-form.tsx
@@ -1,14 +1,108 @@
+"use client";
+
+import { FormTextInput } from "@/app/_components/base/form/text-input";
+import { EmailAutocomplete } from "@/app/_components/email-autocomplete";
+import { GoogleIcon } from "@/app/_components/icons/google-icon";
+import { useColorScheme } from "@/hooks/use-color-scheme";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Button, Divider, Stack, createStyles } from "@mantine/core";
+import { GithubIcon } from "@mantine/ds";
 import { useTranslations } from "next-intl";
 import { FC } from "react";
+import { FormProvider, useForm } from "react-hook-form";
+import { z } from "zod";
+import { AuthForm } from "./form";
 
 export const LoginForm: FC = () => {
-  const { t } = useLoginForm();
+  const { t, classes, loginForm, onSubmit } = useLoginForm();
 
-  return <h1>Login Form</h1>;
+  return (
+    <FormProvider {...loginForm}>
+      <AuthForm onSubmit={onSubmit}>
+        <Stack spacing="sm" w="100%">
+          <Stack spacing="xs">
+            <EmailAutocomplete
+              name="email"
+              label={t("email")}
+              placeholder={t("emailPlaceholder")}
+              withAsterisk
+            />
+            <FormTextInput
+              name="password"
+              label={t("password")}
+              placeholder={t("password")}
+              withAsterisk
+            />
+          </Stack>
+          <Stack mt="md">
+            <Button type="submit" variant="gradient">
+              {t("loginAction")}
+            </Button>
+            <Divider />
+            <Stack spacing="xs">
+              <Button
+                size="xs"
+                variant="default"
+                color="gray.2"
+                leftIcon={<GoogleIcon />}
+              >
+                {t("googleAction")}
+              </Button>
+              <Button
+                size="xs"
+                variant="filled"
+                leftIcon={<GithubIcon size={16} />}
+                className={classes.githubBtn}
+              >
+                {t("githubAction")}
+              </Button>
+            </Stack>
+          </Stack>
+        </Stack>
+      </AuthForm>
+    </FormProvider>
+  );
 };
 
 function useLoginForm() {
   const t = useTranslations("auth.login.form");
+  const [{ isDarkTheme }] = useColorScheme();
+  const { classes } = useStyles(isDarkTheme);
 
-  return { t };
+  const tValidation = useTranslations("validation");
+  const validationMessages: Parameters<typeof loginSchema> = [
+    tValidation("required"),
+    tValidation("invalidEmail"),
+  ];
+  const loginForm = useForm<LoginFormValues>({
+    resolver: zodResolver(loginSchema(...validationMessages)),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+  const { handleSubmit } = loginForm;
+
+  const onSubmit = (values: LoginFormValues) => {
+    console.log(values);
+  };
+
+  return { t, classes, loginForm, onSubmit: handleSubmit(onSubmit) };
 }
+
+type LoginFormValues = z.infer<ReturnType<typeof loginSchema>>;
+const loginSchema = (required: string, invalidEmail: string) =>
+  z.object({
+    email: z.string().email(invalidEmail),
+    password: z.string().nonempty(required),
+  });
+
+const useStyles = createStyles((theme, isDarkTheme: boolean) => ({
+  githubBtn: {
+    backgroundColor: theme.colors.dark[isDarkTheme ? 9 : 5],
+    color: theme.white,
+    ":hover": {
+      backgroundColor: theme.colors.dark[isDarkTheme ? 8 : 6],
+    },
+  },
+}));
