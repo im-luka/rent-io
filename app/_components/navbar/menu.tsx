bf761fe1933afe2b5686bca708afe18e7b57c4f7
im-luka <lux.9833@gmail.com>
2023-08-22 16:36:03 +0200

    feat: navbar menu when user is logged in

--- app/_components/navbar/menu.tsx
+++ app/_components/navbar/menu.tsx
@@ -13,45 +13,105 @@ import {
   IconBallpen,
   IconChevronDown,
   IconDoorEnter,
+  IconHeart,
+  IconLogout,
+  IconMessage,
+  IconStar,
+  IconUserEdit,
 } from "@tabler/icons-react";
 import { useTranslations } from "next-intl";
 import { paths } from "@/navigation/paths";
 import { Link } from "../base/link";
 import { useSession } from "next-auth/react";
+import { Typography } from "../base/typography";
 
 export const Menu: FC = () => {
-  const { t, classes } = useMenu();
+  const { t, classes, theme, user } = useMenu();
+
+  // TODO: 🖌️ implement cleaner menu items once other pages are ready & complete
+  const menuItems = user ? (
+    <>
+      <MantineMenu.Item
+        icon={<IconHeart size={14} color={theme.colors.red[6]} stroke={1.5} />}
+      >
+        Liked posts
+      </MantineMenu.Item>
+      <MantineMenu.Item
+        icon={
+          <IconStar size={14} color={theme.colors.yellow[6]} stroke={1.5} />
+        }
+      >
+        Saved posts
+      </MantineMenu.Item>
+      <MantineMenu.Item
+        icon={
+          <IconMessage size={14} color={theme.colors.blue[6]} stroke={1.5} />
+        }
+      >
+        Your comments
+      </MantineMenu.Item>
+      <MantineMenu.Divider />
+      <MantineMenu.Label>{t("accountSettings")}</MantineMenu.Label>
+      <MantineMenu.Item
+        icon={
+          <IconUserEdit size={14} stroke={1.5} color={theme.colors.green[5]} />
+        }
+      >
+        {t("profile")}
+      </MantineMenu.Item>
+      <MantineMenu.Item
+        icon={<IconLogout size={14} stroke={1.5} color={theme.colors.red[5]} />}
+      >
+        {t("signOut")}
+      </MantineMenu.Item>
+    </>
+  ) : (
+    <>
+      <MantineMenu.Item icon={<IconDoorEnter size={14} stroke={1.5} />}>
+        <Link href={paths.login()}>{t("login")}</Link>
+      </MantineMenu.Item>
+      <MantineMenu.Item icon={<IconBallpen size={14} stroke={1.5} />}>
+        <Link href={paths.register()}>{t("register")}</Link>
+      </MantineMenu.Item>
+    </>
+  );
 
   return (
     <MantineMenu position="bottom-end">
       <MantineMenu.Target>
         <UnstyledButton>
           <Group className={classes.targetBlock}>
-            <Avatar size="sm" variant="light" radius="xl" color="indigo" />
+            <Avatar
+              size="sm"
+              variant="light"
+              radius="xl"
+              color="indigo"
+              src={user?.image}
+            />
+            {user && (
+              <Typography size="sm">
+                {t.rich("userData", {
+                  firstName: user?.firstName,
+                  lastName: user?.lastName,
+                })}
+              </Typography>
+            )}
             <IconChevronDown size={14} />
           </Group>
         </UnstyledButton>
       </MantineMenu.Target>
-      <MantineMenu.Dropdown>
-        <MantineMenu.Item icon={<IconDoorEnter size={14} stroke={1.5} />}>
-          <Link href={paths.login()}>{t("login")}</Link>
-        </MantineMenu.Item>
-        <MantineMenu.Item icon={<IconBallpen size={14} stroke={1.5} />}>
-          <Link href={paths.register()}>{t("register")}</Link>
-        </MantineMenu.Item>
-      </MantineMenu.Dropdown>
+      <MantineMenu.Dropdown>{menuItems}</MantineMenu.Dropdown>
     </MantineMenu>
   );
 };
 
 function useMenu() {
   const t = useTranslations("navbar.menu");
+  const { classes, theme } = useStyles();
   const { data: session } = useSession();
-  const { classes } = useStyles();
+  const user = session?.user;
 
-  console.log(session?.user?.firstName);
-
-  return { t, classes };
+  return { t, classes, theme, user };
 }
 
 const useStyles = createStyles((theme) => ({
