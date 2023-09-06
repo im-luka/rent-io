9da4c36aa8f1a15ef043493875c94fb3ad959117
im-luka <lux.9833@gmail.com>
2023-09-06 18:41:11 +0200

    feat: light/dark mode map layers & custom map styles

--- app/_components/add-property-modal/property-map.tsx
+++ app/_components/add-property-modal/property-map.tsx
@@ -3,10 +3,14 @@
 import { forwardRef } from "react";
 import type { LatLngTuple, Map } from "leaflet";
 import { MapContainer, TileLayer } from "react-leaflet";
+import { createStyles } from "@mantine/core";
+import { useColorScheme } from "@/hooks/use-color-scheme";
 import {
   COUNTRY_MAP_DEFAULT_ZOOM,
   COUNTRY_MAP_MAX_ZOOM,
   COUNTRY_MAP_MIN_ZOOM,
+  MAP_TILE_LAYER_DARK,
+  MAP_TILE_LAYER_LIGHT,
 } from "@/utils/constants";
 
 type Props = {
@@ -15,12 +19,15 @@ type Props = {
 export type PropertyMapProps = Props;
 
 export const PropertyMap = forwardRef<Map, Props>(function PropertyMap(
-  { center },
+  props,
   ref
 ) {
+  const { classes, center, tileLayerUrl } = usePropertyMap(props);
+
   return (
     <MapContainer
       ref={ref}
+      className={classes.map}
       center={center}
       attributionControl={false}
       zoomControl={false}
@@ -28,16 +35,27 @@ export const PropertyMap = forwardRef<Map, Props>(function PropertyMap(
       zoom={COUNTRY_MAP_DEFAULT_ZOOM}
       minZoom={COUNTRY_MAP_MIN_ZOOM}
       maxZoom={COUNTRY_MAP_MAX_ZOOM}
-      style={{
-        width: "100%",
-        height: "100%",
-        position: "absolute",
-        left: 0,
-        top: 0,
-        borderRadius: "8px",
-      }}
     >
-      <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+      <TileLayer url={tileLayerUrl} />
     </MapContainer>
   );
 });
+
+function usePropertyMap({ center }: Props) {
+  const { classes } = useStyles();
+  const [{ isDarkTheme }] = useColorScheme();
+  const tileLayerUrl = isDarkTheme ? MAP_TILE_LAYER_DARK : MAP_TILE_LAYER_LIGHT;
+
+  return { classes, center, tileLayerUrl };
+}
+
+const useStyles = createStyles((theme) => ({
+  map: {
+    width: "100%",
+    height: "100%",
+    position: "absolute",
+    left: 0,
+    top: 0,
+    borderRadius: theme.radius.md,
+  },
+}));
--- utils/constants.ts
+++ utils/constants.ts
@@ -8,14 +8,19 @@ export const PASSWORD_HASH_SALT = 12;
 export const TOKEN_CRYPTO_DEFAULT = 32;
 export const SEARCH_CATEGORIES_KEY = "K";
 export const DEFAULT_CATEGORY_EMOJI = "🏡";
-
-// 🖼️ UI
 export const MIN_PASSWORD_CHARS = 8;
 export const NOTIFICATIONS_LIMIT = 5;
-export const MODAL_OVERLAY_Z_INDEX = 1001;
-export const NOTIFICATION_Z_INDEX = 9999;
-export const COUNTRY_SELECT_Z_INDEX = 1000;
+
+// 🖼️ UI
+export const MAP_TILE_LAYER_DARK =
+  "https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png";
+export const MAP_TILE_LAYER_LIGHT =
+  "https://tiles.stadiamaps.com/tiles/outdoors/{z}/{x}/{y}{r}.png";
 export const COUNTRY_MAP_DEFAULT_ZOOM = 8;
 export const COUNTRY_MAP_MIN_ZOOM = 3;
 export const COUNTRY_MAP_MAX_ZOOM = 13;
 export const DEFAULT_MAP_LATLNG = [51.505, -0.09];
+
+export const MODAL_OVERLAY_Z_INDEX = 1001;
+export const NOTIFICATION_Z_INDEX = 9999;
+export const COUNTRY_SELECT_Z_INDEX = 1000;
