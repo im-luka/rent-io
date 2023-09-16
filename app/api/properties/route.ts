8779068391d4f2f453a668b696527a3367eb9a2e
im-luka <lux.9833@gmail.com>
2023-09-16 11:41:04 +0200

    feat: random image filename

--- app/api/properties/route.ts
+++ app/api/properties/route.ts
@@ -1,10 +1,22 @@
 import { NextResponse } from "next/server";
 import { PropertyData } from "@/domain/types/property-data";
+import { prisma } from "@/domain/db/prisma-client";
 
 export async function POST(request: Request) {
   const data: PropertyData = await request.json();
   // return NextResponse.json("custom.noEmail", { status: 400 });
-  console.log(data);
 
-  return NextResponse.json({ message: "OK!" });
+  console.log(request);
+
+  try {
+    // await prisma.property.create({
+    //   data: {
+
+    //   }
+    // })
+
+    return NextResponse.json({ message: "OK!" });
+  } catch (error) {
+    return NextResponse.json("custom.addProperty", { status: 400 });
+  }
 }
